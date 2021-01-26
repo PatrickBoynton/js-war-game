b3078fbf26dda7f0658c5e4f3f3c6ae21078afe2
PatrickBoynton <jpboynton3@gmail.com>
2021-01-25 21:32:01 -0500

    Rewrote the logic again. Works other htan the very end.

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function Deck() {
   }
 }
 
+
 function shuffle(array) {
   // reverses the sort.
   // array.sort(() => Math.random() - 1);
@@ -48,87 +49,63 @@ const player2Hand = cards.splice(0, 26);
 
 let player1Cards = [];
 let player2Cards = [];
+
 let player1Card = player1Hand.pop();
 let player2Card = player2Hand.pop();
-
-player1Cards.push(player1Card);
-player2Cards.push(player2Card);
+let i = 100;
 
 while (player1Hand.length > 0 || player2Hand.length > 0) {
   if (player1Card.value === player2Card.value) {
-    console.log("War!");
-    console.log(`Player 1 had: ${player1Card.value} of ${player1Card.suit}`);
-    console.log(`Player 2 had: ${player2Card.value} of ${player2Card.suit}`);
+    let player1NewCard = player1Hand.pop();
+    let player2NewCard = player2Hand.pop();
+    let player1WarChest = player1Hand.splice(0, 3);
+    let player2WarChest = player2Hand.splice(0, 3);
 
-    // const player1WarChest = player1Hand.splice(0, 3);
-    // const player2WarChest = player2Hand.splice(0, 3);
-    //
-    // player1WarChest.forEach((item) => {
-    //   player1Cards.push(item);
-    // });
-    //
-    // player2WarChest.forEach((item) => {
-    //   player2Cards.push(item);
-    // });
-    //
-    // const player1NewCard = player1Hand.pop();
-    // const player2NewCard = player2Hand.pop();
-    
-    console.log("-------------------------");
-    console.log(`Player 1 had: ${player1NewCard.value} of ${player2NewCard.suit}`);
-    console.log(`Player 2 had: ${player2NewCard.value} of ${player2NewCard.suit}`);
+  if (player1NewCard.value > player2NewCard.value) {
+    player1WarChest.forEach((item) => {
+      console.log(item);
+      player1Hand.unshift(item);
+    });
 
-    if (player1NewCard.value === player2NewCard.value) {
-      console.log("War again!");
-      break;
-    } else if(player1NewCard.value > player2NewCard.value) {
-
-      player1Cards.forEach((item) => {
-        player1Hand.unshift(item);
-      });
-
-      player2Cards.forEach((item) => {
-        player1Hand.unshift(item);
-      });
-      player1Hand.unshift(player1NewCard, player2NewCard);
-      player1Hand.unshift(player1Card, player2Cards);
-      console.log(player1Hand.length);
-      console.log(player2Hand.length);
-      console.log(player1Hand.length + player2Hand.length);
-    } else if(player1NewCard.value < player2NewCard.value) {
-      player2Cards.forEach((item) => {
-        player2Hand.unshift(item);
-      });
-      player1Cards.forEach((item) => {
-        player2Hand.unshift(item);
-      });
-        player1Hand.unshift(player1NewCard, player2NewCard);
-        player2Hand.unshift(player1Card, player2Cards);
-      console.log(player1Hand.length + player2Hand.length);
-    }
+    player2WarChest.forEach((item) => {
+      console.log(item);
+      player1Hand.unshift(item);
+    });
+    player1Hand.unshift(player1Card, player2Card, player1NewCard, player2NewCard);
 
     console.log(player1Hand.length + player2Hand.length);
-    break;
+  } else {
+    player1WarChest.forEach((item) => {
+      console.log(item);
+      player2Hand.unshift(item);
+    });
+
+    player2WarChest.forEach((item) => {
+      console.log(item);
+      player2Hand.unshift(item);
+    });
+    player2Hand.unshift(player1Card, player2Card, player1NewCard, player2NewCard);
+    console.log(player1Hand.length + player2Hand.length);
+  }
+  console.log(player1Hand.length + player2Hand.length);
   } else if (player1Card.value > player2Card.value) {
     console.log(`Player 1 won: ${player1Card.value} of ${player1Card.suit} and ${player2Card.value} of ${player2Card.suit}`);
-    const player1CardToRemove = player1Cards.splice(0, 1);
-    const player2CardToRemove = player2Cards.splice(0, 1);
 
-    player1Hand.unshift(player1CardToRemove, player2CardToRemove);
+    player1Hand.unshift(player1Card, player2Card);
     console.log(player1Hand.length);
     console.log(player2Hand.length);
     console.log(player1Hand.length + player2Hand.length);
   } else if(player1Card.value < player2Card.value){
     console.log(`Player 2 won: ${player1Card.value} of ${player1Card.suit} and ${player2Card.value} of ${player2Card.suit}`);
-    const player1CardToRemove = player1Cards.splice(0, 1);
-    const player2CardToRemove = player2Cards.splice(0, 1);
 
-    player2Hand.unshift(player1CardToRemove, player2CardToRemove);
+    player2Hand.unshift(player2Card, player2Card);
     console.log(player1Hand.length);
     console.log(player2Hand.length);
     console.log(player1Hand.length + player2Hand.length);
   }
-  player1Card = player1Hand.pop();
-  player2Card = player2Hand.pop();
+  if (player1Hand.length + player2Hand.length === 52) {
+    player1Card = player1Hand.pop();
+    player2Card = player2Hand.pop();
+  }
 
 }
